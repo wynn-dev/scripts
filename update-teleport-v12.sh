3275a45dd15eaa2186159591c3a193e8f68e2703
wynn <66362261+wynn-dev@users.noreply.github.com>
2023-08-24 22:21:30 +0800

    Update and rename update-teleport.sh to update-teleport-v12.sh

--- update-teleport-v12.sh
+++ update-teleport-v12.sh
@@ -4,7 +4,7 @@ sudo curl https://apt.releases.teleport.dev/gpg \
 source /etc/os-release
 
 echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] \
-  https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v11" \
+  https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v12" \
 | sudo tee /etc/apt/sources.list.d/teleport.list > /dev/null
 
 sudo apt-get update
